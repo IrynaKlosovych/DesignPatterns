904fb56aefef9bddec3253a5ec5d2ec300439202
Iryna Klosovych <ipz221_kia@student.ztu.edu.ua>
2024-05-28 14:39:26 +0300

    state ended

---
+++ lab5/State/HiddenState.cs
@@ -0,0 +1,32 @@
+﻿using lab5.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab5.State
+{
+    public class HiddenState: IVisibilityState
+    {
+        private LightElementNode context;
+        public HiddenState(LightElementNode context)
+        {
+            this.context = context;
+        }
+
+        public void SetContext(LightElementNode context)
+        {
+            this.context = context;
+        }
+        public void Hide()
+        {
+            Console.WriteLine($"{context.GetTagName()} is hidden already");
+        }
+        public void Show()
+        {
+            this.context.ChangeState(new VisibleState(context));
+            Console.WriteLine($"{context.GetTagName()} is shown now");
+        }
+    }
+}
---
+++ lab5/State/VisibleState.cs
@@ -0,0 +1,32 @@
+﻿using lab5.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab5.State
+{
+    public class VisibleState: IVisibilityState
+    {
+        private LightElementNode context;
+        public VisibleState(LightElementNode context)
+        {
+            this.context = context;
+        }
+
+        public void SetContext(LightElementNode context)
+        {
+            this.context = context;
+        }
+        public void Hide()
+        {
+            this.context.ChangeState(new HiddenState(context));
+            Console.WriteLine($"{context.GetTagName()} is hidden now");
+        }
+        public void Show()
+        {
+            Console.WriteLine($"{context.GetTagName()} is shown already");
+        }
+    }
+}
